16024faf87130cfeeef0365e6b5089be5efa3b3d
Domenico Pascucci <pasmimmo@gmail.com>
2022-03-17 22:15:56 +0100

    Added more converter to utility class (DepartmentRead, DepartmentDB and DepartmentEntity)

--- src/main/java/eu/ecomind/interview/web/controller/utils/ConverterUtils.java
+++ src/main/java/eu/ecomind/interview/web/controller/utils/ConverterUtils.java
@@ -1,5 +1,6 @@
 package eu.ecomind.interview.web.controller.utils;
 
+import eu.ecomind.interview.db.entities.DepartmentDb;
 import eu.ecomind.interview.web.model.DepartmentRead;
 import eu.ecomind.interview.domain.entities.DepartmentEntity;
 import org.springframework.beans.BeanUtils;
@@ -12,5 +13,27 @@ public class ConverterUtils {
         DepartmentRead departmentWeb = new DepartmentRead();
         BeanUtils.copyProperties(departmentEntity, departmentWeb);
         return departmentWeb;
+    }
+
+    public static DepartmentEntity webToDepartment(DepartmentRead departmentRead){
+        DepartmentEntity entity = new DepartmentEntity();
+        BeanUtils.copyProperties(departmentRead,entity);
+        return entity;
+    }
+
+    public static DepartmentDb pojoToEntity (DepartmentEntity pojo){
+        var converted = new DepartmentDb();
+        BeanUtils.copyProperties(pojo,converted);
+        return converted;
+    }
+
+    public static DepartmentEntity entityToPojo (DepartmentDb entity){
+        var converted = new DepartmentEntity();
+        BeanUtils.copyProperties(entity,converted);
+        return converted;
+    }
+
+    public static DepartmentDb webToEntity(DepartmentRead departmentRead){
+        return pojoToEntity(webToDepartment(departmentRead));
     }
 }
